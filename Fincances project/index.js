87dda2bb2035f559718fa5d68cb66351005b20ab
Castleexe <dipansh860@gmail.com>
2023-07-29 17:36:35 -0700

    intital

---
+++ Fincances project/index.js
@@ -0,0 +1,7 @@
+function calculate()
+{   
+    let salaryAmt = document.getElementById("SalaryAmount").value;
+    let salaryrRate = document.getElementById("SalaryRate").value;
+    console.log(salaryAmt);
+    console.log(salaryrRate);
+}
